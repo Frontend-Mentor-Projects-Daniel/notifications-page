4e84f5988d8b9a329de09a47efc4e79748d67bfb
Daniel Arzanipour <daniel.arzanipour@gmail.com>
2022-12-24 17:14:40 -0500

    initial commit

---
+++ src/elm/Main.elm
@@ -0,0 +1,55 @@
+module Main exposing (..)
+
+import Browser
+import Html exposing (..)
+import Html.Attributes exposing (id)
+import Html.Events exposing (..)
+
+
+main : Program () Model Msg
+main =
+    Browser.sandbox { init = init, update = update, view = view }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { placeHolderValue : String
+    }
+
+
+init : Model
+init =
+    { placeHolderValue = "Hello World!"
+    }
+
+
+
+-- UPDATE
+
+
+type Msg
+    = DoNothing
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        DoNothing ->
+            model
+
+
+
+-- VIEW
+
+
+view : Model -> Html msg
+view model =
+    div [ id "root" ]
+        [ header []
+            [ h1 [] [ text "Hello World!" ]
+            ]
+        , main_ [] []
+        ]
